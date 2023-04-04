aa42fb909c673815475af53bacd9b9b2859a147a
Ivan Rybachok <blackhusky2014@gmail.com>
2023-04-04 18:20:52 -0300

    first commit

---
+++ my-app/src/components/NoteForm.tsx
@@ -0,0 +1,103 @@
+import { Form, Stack, Row, Col, Button } from "react-bootstrap";
+import CreatableReactSelect from "react-select/creatable";
+import { Link, useNavigate } from "react-router-dom";
+import { FormEvent, useRef, useState } from "react";
+import { NoteData, Tag } from "../App";
+import { v4 as uuidV4 } from "uuid";
+
+type NoteFormProps = {
+  // when we pass data we will get nothing in return
+  onSubmit: (data: NoteData) => void;
+  onAddTag: (tag: Tag) => void;
+  availableTags: Tag[];
+} & Partial<NoteData>;
+
+function NoteForm({
+  onSubmit,
+  onAddTag,
+  availableTags,
+  title = "",
+  markdown = "",
+  tags = [],
+}: NoteFormProps) {
+  const titleRef = useRef<HTMLInputElement>(null);
+  const markdownRef = useRef<HTMLTextAreaElement>(null);
+  const [selectedTags, setSelectedTags] = useState<Tag[]>(tags);
+  const navigate = useNavigate();
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    onSubmit({
+      // ! saying that it could be null or never will be null
+      // the reason why because we made it always required so there is going to be some type of value
+      title: titleRef.current!.value,
+      markdown: markdownRef.current!.value,
+      tags: selectedTags,
+    });
+
+    navigate("..");
+  }
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Stack gap={4}>
+        <Row>
+          <Col>
+            <Form.Group controlId="title">
+              <Form.Label>Title</Form.Label>
+              <Form.Control ref={titleRef} required defaultValue={title} />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group controlId="tags">
+              <Form.Label>Tags</Form.Label>
+              <CreatableReactSelect
+                onCreateOption={(label) => {
+                  const newTag = { id: uuidV4(), label };
+                  onAddTag(newTag);
+                  setSelectedTags((prev) => [...prev, newTag]);
+                }}
+                value={selectedTags.map((tag) => {
+                  return { label: tag.label, value: tag.id };
+                })}
+                options={availableTags.map((tag) => {
+                  return { label: tag.label, value: tag.id };
+                })}
+                onChange={(tags) => {
+                  setSelectedTags(
+                    tags.map((tag) => {
+                      return { label: tag.label, id: tag.value };
+                    })
+                  );
+                }}
+                isMulti
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+        <Form.Group controlId="markdown">
+          <Form.Label>Body</Form.Label>
+          <Form.Control
+            defaultValue={markdown}
+            required
+            as="textarea"
+            ref={markdownRef}
+            rows={15}
+          />
+        </Form.Group>
+        <Stack direction="horizontal" gap={2} className="justify-content-end">
+          <Button type="submit" variant="primary">
+            Save
+          </Button>
+          <Link to="..">
+            <Button type="button" variant="outline-secondary">
+              Cancel
+            </Button>
+          </Link>
+        </Stack>
+      </Stack>
+    </Form>
+  );
+}
+
+export default NoteForm;
